b9b1861e99283ab2b3b60dc69e78f5f6f4670e47
marks <marks@pop-os.localdomain>
2023-09-15 10:55:47 -0400

    temp changes

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
 #include <cstring>
+#include <mutex>
+#include <queue>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <thread>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include "TrafficLight.h"
 
 int handleClient(int client_socket) {
     close(client_socket);
+    return 0;
 }
 
 
+struct Message {
+    int clientId;
+    std::string content;
+};
+
+void trafficLightController(std::mutex& mutex, std::queue<Message>& messageQueue) {
+    while(true) {
+        // Check if there is a queued cycle to execute
+        // It not, check the message queue
+        std::unique_lock<std::mutex> lock(mutex);
+        if (messageQueue.empty()) {
+            // No queued cycles or messaged, continue with current cycle.
+            lock.unlock();
+            //Implement traffic logic
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+}
+
 int main() {
-    
+    std::queue<Message> messageQueue;
+    std::mutex mutex;
+
+    TrafficLight NB = TrafficLight();
+    TrafficLight SB = TrafficLight();
+    TrafficLight EB = TrafficLight();
+    TrafficLight WB = TrafficLight();
+
     const char* server_ip = "127.0.0.1";
     const int server_port = 12345;
+    const int MAX_CLIENTS = 4;
 
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
@@ -46,6 +78,9 @@ int main() {
     }
 
     std::cout << "Server is listening on " << server_ip << ":" << server_port << std::endl;
+
+    // Start a thread for the traffic light controller
+    std::thread controllerThread(trafficLightController);
 
     while(true) {
         struct sockaddr_in client_address;
